ac6e0adb1562bbb23d8be39aa3fbd7caec240676
ellajectey <emmanuella.jecteyasare@gmail.com>
2024-02-27 12:18:30 +0000

    searching for items using the enter key

--- src/pages/recipes/index.jsx
+++ src/pages/recipes/index.jsx
@@ -7,13 +7,19 @@ export default function Recipes() {
 
 // don't use variables in react use useState
    const[recipes,setRecipes] = useState([]);
+   const [searchItem,setSearchItem] = useState("");
 
    const searchRecipes =() => {
 
     // prepare url
     const url =new URL ('https://api.spoonacular.com/recipes/complexSearch')
-    url.searchParams.append('apiKey','efc2e52d81844383846f9132b7574648')
-    //fetch recipes
+    url.searchParams.append('apiKey',process.env.REACT_APP_SPOONACULAR_API_KEY);
+
+    // searching for the item in the api
+    url.searchParams.append('query',searchItem); //add the query parameter
+    url.searchParams.append('number',20); // changes the number of items showing in the default page
+
+    //fetch recipes from API
     fetch(url)
     .then((response) => response.json())
     .then((data)=>{
@@ -31,11 +37,15 @@ export default function Recipes() {
 
     return (
         <>
-            <Container sx={{ my: '2rem' }}>
-                <TextField fullWidth
+            <Container  sx={{ my: '2rem' }}>
+                <TextField color="warning" fullWidth
                     id="outlined-basic"
                     label="Enter a keyword to search recipes and hit Enter"
-                    variant="outlined" />
+                    variant="outlined" 
+                    value={searchItem}
+                    onChange={(event) =>setSearchItem(event.target.value)}
+                    onKeyDown ={event => event.key =="Enter" && searchRecipes()}
+                    />
                 <Grid sx={{ mt: '2rem' }} container spacing={3}>
                     {recipes.map((recipe) => <RecipeItem key={recipe.id} title={recipe.title} image={recipe.image}/>)}
                 </Grid>
